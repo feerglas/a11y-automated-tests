afcaa4bec2680d467edf7e237cbb5f0703bed2c7
Yves Tscherry <info@feerglas.com>
2025-05-07 13:07:01 +0200

    style: minor style adoptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <main>
         <h1>Automated a11y testing with Playwright</h1>
         <p>Some sample scenarios for automated a11y E2E testing using Playwright.</p>
 
-        <h2>AXE</h2>
-        <p>Some tests using the AXE tool: <Link href='/axe'>AXE Tests</Link>.</p>
+        <section className={styles.section}>
+          <h2>AXE</h2>
+          <p>Some tests using the AXE tool: <Link href='/axe'>AXE Tests</Link>.</p>
+        </section>
 
-        <h2>Snapshot testing</h2>
-        <p>Some tests asserting on the accessibility tree: <Link href='/snapshot'>Snapshots on accessibility tree</Link></p>
+        <section className={styles.section}>
+          <h2>Snapshot testing</h2>
+          <p>Some tests asserting on the accessibility tree: <Link href='/snapshot'>Snapshots on accessibility tree</Link></p>
+        </section>
       </main>
     </div>
   );
