7834cbe89122330cb59b556ad476bbfa8bbcb7e8
Yves Tscherry <info@feerglas.com>
2025-05-07 10:22:41 +0200

    test: add sample axe errors

--- src/app/axe/page.tsx
+++ src/app/axe/page.tsx
@@ -8,6 +8,23 @@ export default function Axe() {
       <main>
         <h1>AXE</h1>
         <p>See <a href="https://playwright.dev/docs/accessibility-testing">https://playwright.dev/docs/accessibility-testing</a> for more information.</p>
+
+        <h2>Lists</h2>
+        <p>Error: Ensure that lists are structured correctly.</p>
+        <ul>
+          <li>List item 1</li>
+          <li>List item 2</li>
+          <li>List item 3</li>
+
+          {/* ERROR */}
+          {/* <p>List item 4</p> */}
+        </ul>
+
+        <h2>Interactive controls nesting</h2>
+        <p>Error: Ensure interactive controls are not nested as they are not always announced by screen readers or can cause focus problems for assistive technologies.</p>
+
+        {/* ERROR */}
+        {/* <button><a href="https://google.com">Some Link</a></button> */}
       </main>
     </div>
   );
--- tests/axe.spec.ts
+++ tests/axe.spec.ts
@@ -5,7 +5,8 @@ test.describe('homepage', () => {
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     await page.goto('http://localhost:3000/axe');
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .analyze();
 
     expect(accessibilityScanResults.violations).toEqual([]);
   });
