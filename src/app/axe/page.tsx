f23f952c05463211ddee91d754284f75b29b1336
Yves Tscherry <info@feerglas.com>
2025-05-07 10:57:47 +0200

    test: add some more test examples

--- src/app/axe/page.tsx
+++ src/app/axe/page.tsx
@@ -9,22 +9,65 @@ export default function Axe() {
         <h1>AXE</h1>
         <p>See <a href="https://playwright.dev/docs/accessibility-testing">https://playwright.dev/docs/accessibility-testing</a> for more information.</p>
 
-        <h2>Lists</h2>
-        <p>Error: Ensure that lists are structured correctly.</p>
-        <ul>
-          <li>List item 1</li>
-          <li>List item 2</li>
-          <li>List item 3</li>
+        <section>
+          <h2>Lists</h2>
+          <p>Error: Ensure that lists are structured correctly.</p>
+          <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+
+            {/* ERROR */}
+            {/* <p>List item 4</p> */}
+          </ul>
+        </section>
+
+        <section>
+          <h2>Interactive controls nesting</h2>
+          <p>Error: Ensure interactive controls are not nested as they are not always announced by screen readers or can cause focus problems for assistive technologies.</p>
+
+          <button>
+            Some button text
+
+            {/* ERROR */}
+            {/* <a href="https://google.com">Some Link</a> */}
+          </button>
+        </section>
+
+        <section>
+          <h2>Roles</h2>
+          <p>Error: Ensure all elements with a role attribute use a valid value.</p>
+          {/* ERROR */}
+          {/* <p role="foo"> */}
+
+          <p role="alert">
+            This text has an invalid role attribute
+          </p>
+        </section>
+
+        <section>
+          <h2>Color Contrast</h2>
+          <p>Error: Ensure the contrast between foreground and background colors meets WCAG 2 AA minimum contrast ratio thresholds.</p>
 
           {/* ERROR */}
-          {/* <p>List item 4</p> */}
-        </ul>
+          {/* <p style={{ color: '#A2A2A2'}}> */}
 
-        <h2>Interactive controls nesting</h2>
-        <p>Error: Ensure interactive controls are not nested as they are not always announced by screen readers or can cause focus problems for assistive technologies.</p>
+          <p>
+            The color contrast of this text is too low
+          </p>
+        </section>
 
-        {/* ERROR */}
-        {/* <button><a href="https://google.com">Some Link</a></button> */}
+
+        <section>
+          <h2>Form elements</h2>
+          <p>Error: Ensure every form element has a label.</p>
+
+          {/* ERROR */}
+          {/* <label>Some label</label> */}
+
+          <label htmlFor="form-elem">Some label</label>
+          <input type="text" id="form-elem"/>
+        </section>
       </main>
     </div>
   );
