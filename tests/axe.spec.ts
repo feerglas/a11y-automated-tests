9459ee349ad8ae725673e7dae6e1b7df82107350
Yves Tscherry <info@feerglas.com>
2025-05-07 16:51:58 +0200

    test: remove obsolete describe block

--- tests/axe.spec.ts
+++ tests/axe.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-test.describe('homepage', () => {
-  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto('http://localhost:3000/axe');
+test('Page should not have any automatically detectable accessibility issues', async ({ page }) => {
+  await page.goto('http://localhost:3000/axe');
 
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .analyze();
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .analyze();
 
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
